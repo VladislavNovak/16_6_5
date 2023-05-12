f44e29f42dc40be048951034ace6e56a3c801094
Novak <novaldix@gmail.com>
2023-05-12 08:08:06 +0300

    16_6_5 added user input, validate

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <algorithm> // any_of
 #include <string>
 
 using std::cout;
 using std::endl;
 using std::string;
 
-enum Switches {
+enum DeviceStatus {
     MAIN = 1,
     INSIDE_LIGHT = 2,
     OUTSIDE_LIGHT = 4,
@@ -15,8 +16,105 @@ enum Switches {
     GARDEN_LIGHTING = 64,
 };
 
+// Получаем true если элемент `item` хоть раз встречается в диапазоне `range`
+bool isIncludes(const string &range, const char &item) {
+    return std::any_of(range.begin(),
+                       range.end(),
+                       [&item](const char &c) { return c == item; });
+}
+
+// Получаем char-символ в обозначенном диапазоне `range`
+template<typename T>
+T getUserChar(string const &range) {
+    T input;
+
+    while (true) {
+        std::cin >> input;
+        // Если введено значение из диапазона - сбрасываем
+        if (isIncludes(range, input)) break;
+        printf("Error. Symbol %c not in range: %s. Repeat: ", input, range.c_str());
+        // Сбрасываем коматозное состояние cin
+        std::cin.clear();
+        // Очищаем поток ввода
+        fflush(stdin);
+    }
+
+    return input;
+}
+
+// Получаем слово с параметрами:
+//  - в указанном диапазоне `range`
+//  - разрешено ли повторное введение символов. По умолчанию - не разрешено
+//  - не более определенной длины (`maxCount`),
+//  - с окончанием для ввода (`INPUT_ENDING`),
+string getUserInput(const string &range, bool isRepeatAllowed = false, size_t maxCount = 128) {
+    char const INPUT_ENDING = '.';
+    string constraints = range;
+    string userInput;
+    size_t curCount = 0;
+
+    constraints.append(1, INPUT_ENDING);
+
+    // to format: zu - size_t, s - string.c_str, c - char
+    printf("Enter up characters in the range: %s\n", constraints.c_str());
+    printf("To complete, enter %zu characters or press the key '%c': ", maxCount, INPUT_ENDING);
+    while(curCount < maxCount) {
+        char ch = getUserChar<char>(constraints);
+        if (ch == INPUT_ENDING) break;
+        if (!isRepeatAllowed && isIncludes(userInput, ch)) {
+            printf("Selected: %s. Symbol '%c' is already there. Enter another or press the key '%c': ", userInput.c_str(), ch, INPUT_ENDING);
+            continue;
+        }
+        userInput += ch;
+        ++curCount;
+    }
+
+    return userInput;
+}
+
+// Возвращает пользовательское слово с отсортированными уникальными символами, указанными в диапазоне constraint
+string getUserChoice(size_t const devicesCount) {
+    string constraint;
+    // Набираем лишь символы от 0 до devicesCount
+    for (int i = 0; i < devicesCount; ++i) constraint += (i + '0');
+
+    string userInput = getUserInput(constraint, false, devicesCount);
+    std::sort(userInput.begin(), userInput.end());
+
+    return userInput;
+}
+
+void printReport(
+        int hour,
+        int outsideTemperature,
+        int insideTemperature,
+        int isMotionDetection,
+        const unsigned int* deviceState,
+        const string* deviceNames,
+        const size_t switchCount) {
+    cout << "-----------" << hour << "--------------------\n";
+    cout << "Data outside: \n";
+    printf("Outside temperature: %14i\n", outsideTemperature);
+    printf("Inside temperature:  %14i\n", insideTemperature);
+    printf("Is motion detection: %14i\n", isMotionDetection);
+    cout << "-------------------------------\nCurrent switches status: \n";
+    for (int i = 0; i < switchCount; ++i) {
+        printf("(%i) status of %-*s: %s\n",
+               i,
+               16, deviceNames[i].c_str(),
+               deviceState[i] ? "ON " : "OFF");
+    }
+    cout << "-------------------------------\n";
+}
+
 int main() {
-    string switchNames[] = {
+    const int DEVICES_COUNT = 7;
+
+    int outsideTemperature;
+    int insideTemperature;
+    bool isMotionDetection = false;
+
+    string deviceNames[DEVICES_COUNT] = {
             "all",
             "inside light",
             "outside light",
@@ -26,25 +124,22 @@ int main() {
             "garden lighting"
     };
 
-    unsigned int switchStates[5] = {0};
+    unsigned int deviceState[DEVICES_COUNT] = {0};
+    string userChoice;
 
-    // switchStates[0] &= ~Switches::LIGHTS_INSIDE;
-    // switchStates[1] &= ~Switches::LIGHTS_OUTSIDE;
-    // switchStates[2] &= ~Switches::HEATERS;
-    // switchStates[3] &= ~Switches::WATER_PIPE_HEATING;
-    // switchStates[4] &= ~Switches::CONDITIONER;
+    //------hour 00:00
+    outsideTemperature = 12;
+    insideTemperature = 23;
+    isMotionDetection = false;
 
+    printReport(0, outsideTemperature, insideTemperature, isMotionDetection, deviceState, deviceNames, DEVICES_COUNT);
 
-    // // Включить. Результат: 4
-    // switchState = switchState | Switches::HEATERS;
-    // cout << switchState << endl;
-    //
-    // // Выключить. Результат: 0
-    // switchState = switchState & ~Switches::HEATERS;
-    // cout << switchState << endl;
+    cout << "Do you want to enable/disable devices?\n";
+    cout << "Enter 'Y' to on/off devices or 'N' to skip until next hour: ";
+    char YES_NO = getUserChar<char>("YyNn");
+    if (YES_NO == 'Y' || YES_NO == 'y') {
+        userChoice = getUserChoice(DEVICES_COUNT);
 
-    for (auto i : switchStates) {
-        cout << i << endl;
+        cout << userChoice << endl;
     }
-
 }
