2d8333153dccc2b5733f01e655556a135d2e3611
Novak <novaldix@gmail.com>
2023-05-22 18:37:13 +0300

    16_6_5 added functionality

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ using std::vector;
 
 enum class ExternalType { TIME, OUTSIDE_TEMPERATURE, INSIDE_TEMPERATURE, MOTION_DETECTION, COUNT };
 
+// Сюда можно помещать дополнительные новые устройства
 enum class InternalType { BRIGHTNESS, COUNT };
 
 // операции добавления/удаления/инверсии
@@ -38,6 +39,8 @@ vector<string> switchNames = {
         "conditioner",
         "garden lighting"
 };
+
+vector<string> InternalNames = { "brightness" };
 
 // Получаем true если элемент `item` хоть раз встречается в диапазоне `range`
 bool isIncludes(const string &range, const char &item) {
@@ -98,7 +101,7 @@ string getUserInput(const string &range, bool isRepeatAllowed = false, size_t ma
     constraints.append(1, TERMINATOR);
 
     // to format: zu - size_t, s - string.c_str, c - char
-    printf("Enter%s symbols in range: %s ", (isRepeatAllowed ? "" : " unique"), getJoinRange(range).c_str());
+    printf("Enter%s numbers in range: %s ", (isRepeatAllowed ? "" : " unique"), getJoinRange(range).c_str());
     printf("To complete - press the key '%c': ", TERMINATOR);
     while (curCount < maxCount) {
         char ch = getUserChar<char>(constraints);
@@ -163,9 +166,19 @@ bool hasToggleFlag(ToggleType item, const unsigned int &store) {
 
 // Получить отчет о бите в формате "status_bit : name_bit"
 string getToggleInfo(ToggleType item, const unsigned int &store) {
-    string switchInfo = hasToggleFlag(item, store) ? "ON:  " : ": OFF: ";
+    string switchInfo = hasToggleFlag(item, store) ? "ON:  " : "OFF: ";
     switchInfo += switchNames[getTogglePosition(item)];
     return switchInfo;
+}
+
+string getInternalInfo(const int* internalData, InternalType itemPosition) {
+    char buffer[255];
+    int len = sprintf(buffer,
+                      "%i: %s",
+                      internalData[static_cast<int>(itemPosition)],
+                      InternalNames[static_cast<int>(itemPosition)].c_str());
+    string temp = string(buffer, len);
+    return temp;
 }
 
 bool plumbingController(unsigned int &store, const int* externalData) {
@@ -246,67 +259,50 @@ int getCurrentTime(const int previous, int &start) {
     return current > 23 ? 0 : current;
 }
 
-bool setBrightness(int* internalData, const int* externalData) {
+bool setBrightness(bool isInsideLightOn, int* internalData, const int* externalData) {
     bool isStatusChanges = false;
     const int MAX_BRIGHT = 5000;
     const double REDUCTION = 11.5;
     unsigned int brightness;
     const int time = externalData[static_cast<int>(ExternalType::TIME)];
 
-    if (time >= 0 && time <= 16) brightness = 0;
-    else if (time == 17) brightness = 1;
-    else if (time == 18) brightness = 2;
-    else if (time == 19) brightness = 3;
-    else brightness = 4;
+    if (isInsideLightOn) {
+        if (time >= 0 && time <= 16) brightness = 0;
+        else if (time == 17) brightness = 1;
+        else if (time == 18) brightness = 2;
+        else if (time == 19) brightness = 3;
+        else brightness = 4;
 
-    cout << "I: time:: " << time << endl;
-    cout << "I: flag:: " << brightness << endl;
-    int result = (int)(MAX_BRIGHT - (MAX_BRIGHT * REDUCTION / 100 * brightness));
+        int result = (int)(MAX_BRIGHT - (MAX_BRIGHT * REDUCTION / 100 * brightness));
 
-    if (internalData[static_cast<int>(InternalType::BRIGHTNESS)] != result) {
-        internalData[static_cast<int>(InternalType::BRIGHTNESS)] = result;
-        isStatusChanges = true;
-    }
-
-    return isStatusChanges;
-}
-
-bool setBrightness2(unsigned int &currentBrightness, const int* externalData) {
-    bool isStatusChanges = false;
-    const int MAX_BRIGHT = 5000;
-    const double REDUCTION = 11.5;
-    unsigned int brightness;
-    const int time = externalData[static_cast<int>(ExternalType::TIME)];
-
-    if (time >= 0 && time <= 16) brightness = 0;
-    else if (time == 17) brightness = 1;
-    else if (time == 18) brightness = 2;
-    else if (time == 19) brightness = 3;
-    else brightness = 4;
-
-    unsigned int result = (int)(MAX_BRIGHT - (MAX_BRIGHT * REDUCTION / 100 * brightness));
-
-    if (currentBrightness != result) {
-        currentBrightness = result;
-        isStatusChanges = true;
+        if (internalData[static_cast<int>(InternalType::BRIGHTNESS)] != result) {
+            internalData[static_cast<int>(InternalType::BRIGHTNESS)] = result;
+            isStatusChanges = true;
+        }
+    } else {
+        if (internalData[static_cast<int>(InternalType::BRIGHTNESS)] != 0) {
+            internalData[static_cast<int>(InternalType::BRIGHTNESS)] = 0;
+            isStatusChanges = true;
+        }
     }
 
     return isStatusChanges;
 }
 
 void setExternalData(int *externalData, int &startTime) {
-    externalData[static_cast<int>(ExternalType::TIME)] = getCurrentTime(externalData[static_cast<int>(ExternalType::TIME)], startTime);
+    const int timePosition = static_cast<int>(ExternalType::TIME);
+    externalData[timePosition] = getCurrentTime(externalData[timePosition], startTime);
     externalData[static_cast<int>(ExternalType::OUTSIDE_TEMPERATURE)] = getRandomIntInRange(-10, 40);
     externalData[static_cast<int>(ExternalType::INSIDE_TEMPERATURE)] = getRandomIntInRange(-10, 40);
     externalData[static_cast<int>(ExternalType::MOTION_DETECTION)] = getRandomIntInRange(0, 1);
 }
 
-void printSwitchStorage(const unsigned int &store) {
+void printTogglesBox(const unsigned int &store) {
     const auto SIZE = static_cast<size_t>(ToggleType::COUNT);
 
     assert(SIZE == switchNames.size());
 
-    cout << "------------------------------------\nToggleType status: \n";
+    cout << "-------------CURRENT STATES--------------\n";
     for (int currentToggleNumber = 0; currentToggleNumber < SIZE; ++currentToggleNumber) {
         // - выравнивание по левому краю, * даёт возможность указать длину поля
         printf("(%i) status of %-*s: %s\n",
@@ -317,63 +313,77 @@ void printSwitchStorage(const unsigned int &store) {
 }
 
 void printChangedTogglesInfo(vector<string> &changedTogglesInfo) {
-    cout << "------------------------------------\nList of changed states: \n";
+    cout << "-------------CHANGED STATES--------------\n";
     for (auto &i: changedTogglesInfo) cout << i << endl;
 }
 
-void printExternalData(const int *externalData) {
-    const auto SIZE = static_cast<size_t>(ExternalType::COUNT);
-    vector<string> externalDataNames = {
-            "Time",
-            "Outside temperature",
-            "Inside temperature ",
-            "Is motion detection"
-    };
+void printTime(const int time, bool isPrintTime) {
+    if (isPrintTime) printf("------------------%02i:00------------------\n", time);
+}
 
-    assert(SIZE == externalDataNames.size());
-
-    cout << "------------------------------------\nData outside: \n";
-    for (int i = 0; i < SIZE; ++i) {
-        if (i == static_cast<int>(ExternalType::TIME)) {
-            printf("%-*s: %02i:00\n",
-                   30, externalDataNames[i].c_str(),
-                   externalData[i]);
-        } else {
-            printf("%-*s: %i\n",
-                   30, externalDataNames[i].c_str(),
-                   externalData[i]);
-        }
+void printData(const int* data, vector<string> const &names, const int startPosition = 0) {
+    for (int i = startPosition; i < names.size(); ++i) {
+        printf("%-*s: %i\n", 30, names[i].c_str(), data[i]);
     }
 }
 
-void printReport(const unsigned int &store, const int *externalData) {
+void printExternalData(const int* externalData) {
+    vector<string> externalDataNames = {
+            "TIME",
+            "Temperature outside",
+            "Temperature inside ",
+            "Motion detected    "
+    };
+    const auto SIZE = static_cast<size_t>(ExternalType::COUNT);
+    assert(SIZE == externalDataNames.size());
+    cout << "--------------DATA OUTSIDE---------------\n";
+    printData(externalData, externalDataNames, 1);
+}
+
+void printInternalData(const int* internalData) {
+    vector<string> internalDataNames = { "Brightness         " };
+    const auto SIZE = static_cast<size_t>(InternalType::COUNT);
+    assert(SIZE == internalDataNames.size());
+    cout << "--------------DATA INSIDE----------------\n";
+    printData(internalData, internalDataNames);
+}
+
+void printReport(const unsigned int &store, const int* externalData, const int* internalData, bool isPrintTime) {
+    printTime(externalData[static_cast<int>(ExternalType::TIME)], isPrintTime);
+    printTogglesBox(store);
     printExternalData(externalData);
-    printSwitchStorage(store);
-    cout << "------------------------------------\n";
+    printInternalData(internalData);
+    cout << "-----------------------------------------\n";
 }
 
 int main() {
+    bool EXIT_STATUS = false;
+    // Переключатели (см. ToggleType)
     unsigned int togglesBox;
     resetAllToggles(togglesBox);
+    // Перечень данных получаемых от внешних устройств (см. ExternalType)
     int externalData[static_cast<int>(ExternalType::COUNT)] = {0};
+    // Перечень данных получаемых от внутренних устройств (см. InternalType)
     int internalData[static_cast<int>(InternalType::COUNT)] = {0};
-    unsigned int currentBrightness = 0;
 
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     int startTime = getRandomIntInRange(0, 23);
 
-    while (true) {
+    while (!EXIT_STATUS) {
         vector<string> changedTogglesInfo;
         string userChoice;
 
         setExternalData(externalData, startTime);
+        setBrightness(hasToggleFlag(ToggleType::INSIDE_LIGHT, togglesBox), internalData, externalData);
 
-        printReport(togglesBox, externalData);
+        printReport(togglesBox, externalData, internalData, true);
 
-        if (hasDialogYesNo(!hasToggleFlag(ToggleType::MAIN, togglesBox)
-                         ? "Smart home is off. Turn it on?"
-                         : "Smart home is on. Turn it off?")) {
+        string needOnOffMainToggle = !hasToggleFlag(ToggleType::MAIN, togglesBox)
+                                     ? "Smart home is off. Turn it on?"
+                                     : "Smart home is on. Turn it off?";
+
+        if (hasDialogYesNo(needOnOffMainToggle)) {
             changeToggle(ToggleType::MAIN, OpType::REVERSAL, togglesBox);
             changedTogglesInfo.push_back(getToggleInfo(ToggleType::MAIN, togglesBox));
         }
@@ -401,12 +411,8 @@ int main() {
             bool isConditionerChanges = conditionerController(togglesBox, externalData);
             if (isConditionerChanges) changedTogglesInfo.push_back(getToggleInfo(ToggleType::CONDITIONER, togglesBox));
 
-            if (hasToggleFlag(ToggleType::INSIDE_LIGHT, togglesBox)) {
-                setBrightness(internalData, externalData);
-                // setBrightness(currentBrightness, externalData);
-                cout << "BRI:: " << internalData[static_cast<int>(InternalType::BRIGHTNESS)] << endl;
-                // cout << "BRI:: " << currentBrightness << endl;
-            }
+            bool isBrightnessChanges = setBrightness(hasToggleFlag(ToggleType::INSIDE_LIGHT, togglesBox), internalData, externalData);
+            if (isBrightnessChanges) changedTogglesInfo.push_back(getInternalInfo(internalData, InternalType::BRIGHTNESS));
 
             if (hasToggleFlag(ToggleType::OUTSIDE_LIGHT, togglesBox)) {
                 bool isGardenLightingChanges = gardenLightingController(togglesBox, externalData);
@@ -414,11 +420,15 @@ int main() {
             }
         } else {
             resetAllToggles(togglesBox);
+
+            if (hasDialogYesNo("Do you want to EXIT from program?")) EXIT_STATUS = true;
         }
 
         if (!changedTogglesInfo.empty()) {
             printChangedTogglesInfo(changedTogglesInfo);
-            printReport(togglesBox, externalData);
+            printReport(togglesBox, externalData, internalData, false);
         }
     }
+
+    cout << "The program has ended. See you" << endl;
 }
